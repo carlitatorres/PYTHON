1e09063894eddd670ebef378997dfda71ef983bb
carlitatorres <carla.torres.programacion@gmail.com>
2023-05-02 15:03:39 -0400

    commit de contenido

---
+++ ej.py/ej.py
@@ -0,0 +1,20 @@
+# y = 3
+# while y > 0:
+#     print(y)
+#     y = y - 1
+#     if y == 0:
+#         break
+# else:		# solo se ejecuta en una salida limpia del bucle while (es decir, no un break)
+#    print("sentencia else final")
+# # salida: 3, 2, 1
+
+for i in range(0,151):
+    print(i)
+for i in range (0,1005,5):
+    print(i)
+for x in range (1,101):
+    print (x)
+
+    
+
+
